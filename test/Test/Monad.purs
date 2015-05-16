61b3b03e088f2e7e5b2b5241d482234f6cc2073f
Isaac Shapira <fresheyeball@gmail.com>
2015-05-16 16:48:16 -0700

    more functor break apart

--- test/Test/Monad.purs
+++ test/Test/Monad.purs
@@ -50,37 +50,38 @@ checkFunctorInstance :: forall f a.
   => f a -> QC Unit
 checkFunctorInstance _ = checkFunctorInstance' ((==) :: CustomEq (f a))
 
--- checkApplicative :: forall f a b c.
---   ( Applicative f
---   , Arbitrary (f a)
---   , Arbitrary (f (a -> b))
---   , Arbitrary (f (b -> c))
---   , CoArbitrary a
---   , Arbitrary b
---   , Arbitrary a
---   , Eq (f a)
---   , Eq (f b)
---   , Eq (f c))
---   => f a -> f b -> f c -> QC Unit
--- checkApplicative ta tb tc = do
---   quickCheck $ identity ta
---   quickCheck $ composition ta tb tc
---   quickCheck $ homomorphism ta tb
---   quickCheck $ interchange ta tb
---
---   where
---
---   identity :: f a -> f a -> Boolean
---   identity _ v = (pure id <*> v) == v
---
---   composition ::  f a -> f b -> f c -> f (b -> c) -> f (a -> b) -> f a -> Boolean
---   composition _ _ _ u v w = (pure (<<<) <*> u <*> v <*> w) == (u <*> (v <*> w))
---
---   homomorphism :: f a -> f b -> (a -> b) -> a -> Boolean
---   homomorphism _ tb f x = (pure f <*> pure x) == (pure (f x) `asTypeOf` tb)
---
---   interchange :: f a -> f b -> a -> f (a -> b) -> Boolean
---   interchange _ _ y u = (u <*> pure y) == (pure (\x -> x y) <*> u)
+checkApplicative :: forall f a b c.
+  ( Applicative f
+  , Arbitrary (f a)
+  , Arbitrary (f (a -> b))
+  , Arbitrary (f (b -> c))
+  , CoArbitrary a
+  , Arbitrary b
+  , Arbitrary a
+  , Eq (f a)
+  , Eq (f b)
+  , Eq (f c))
+  => f a -> f b -> f c -> QC Unit
+checkApplicative ta tb tc = do
+  quickCheck identity
+  quickCheck $ composition ta tb tc
+  quickCheck $ homomorphism ta tb
+  quickCheck $ interchange ta tb
+
+  where
+
+  identity :: f a -> Boolean
+  identity v = (pure id <*> v) == v
+
+  composition ::  f a -> f b -> f c -> f (b -> c) -> f (a -> b) -> f a -> Boolean
+  composition _ _ _ u v w = (pure (<<<) <*> u <*> v <*> w) == (u <*> (v <*> w))
+
+  homomorphism :: f a -> f b -> (a -> b) -> a -> Boolean
+  homomorphism _ tb f x = (pure f <*> pure x) == (pure (f x) `asTypeOf` tb)
+
+  interchange :: f a -> f b -> a -> f (a -> b) -> Boolean
+  interchange _ _ y u = (u <*> pure y) == (pure (\x -> x y) <*> u)
+
 --
 -- checkMonad :: forall m a.
 --   ( Monad m
